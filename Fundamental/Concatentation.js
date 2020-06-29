675e8f0b024e1fb0fbf7d053746d889f323d9681
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-06-29 21:43:12 +0430

    Fundamental: Concatentation

---
+++ Fundamental/Concatentation.js
@@ -0,0 +1,6 @@
+const name = 'behzad';
+const age = '22';
+console.log('Hi, I am ' + name + ' and I am ' + age + '.');
+
+const greeting = `Hi, I am ${name} and I am ${age}`;.  //	USE BACKTRICK
+console.log(greeting);
