86d0ce3ffdbd493fbcf4b7022fd3664e7a0a4307
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-06-29 21:49:37 +0430

    Fundamental: Loop

---
+++ Fundamental/Loops.js
@@ -0,0 +1,16 @@
+//	FOR	//
+for (let i = 0; i < todos.lenght; i++) {
+  console.log(todos[i].content);
+}
+
+for (let todo of todos) {
+  console.log(todos[i].content);
+
+}
+
+//	WHILE	//
+let i = 0;
+while (i < todos.lenght) {
+  console.log(todos[i].content);
+  i++;
+}
