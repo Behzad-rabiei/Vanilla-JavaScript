4695f1387ee604d85c887ab6b33f3997308fd835
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-06-29 21:42:25 +0430

    Fundamental: DataTypes

---
+++ Fundamental/DataTypes.js
@@ -0,0 +1,8 @@
+//	STRING, NUMBERS, BOOLEAN, NULL, UNDEFINED	//
+const name = 'Behzad';
+const age = 20;
+const rate = 1.4;
+const isOK = false;
+const kind = null;
+const define = undefined;
+console.log(typeof kind) // EXPECT NULL BUT IT REPRESENT OBJECT THAT IT IS NOT TRUE
