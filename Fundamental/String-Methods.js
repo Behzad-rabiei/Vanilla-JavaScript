6794dfcc6a04d8dd2ed3f7580bd60addde2bf91b
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-06-29 21:44:02 +0430

    Fundamental: String Methods And Properties

---
+++ Fundamental/String-Methods.js
@@ -0,0 +1,9 @@
+const myString = 'Hello World';
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.substring(0, 4).toUpperCase());
+console.log(myString.split(''));
+
+const devices = 'phone, laptop, tablet';
+console.log(devices.split(',')); //	ARRAY WITH 3 VALUES
+console.log(myString.lenght) // THIS IS PROPERTIE
