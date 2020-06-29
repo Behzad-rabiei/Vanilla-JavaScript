e462dd4b147956acce2dea71d9e6fe8cd0689113
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-06-29 21:45:17 +0430

    Fundamental: JSON

---
+++ Fundamental/JSON.js
@@ -0,0 +1,23 @@
+//	USED ONLINE JSON CONVERTER :)
+//  https://www.freeformatter.com/json-formatter.html
+
+[
+  {
+    "id": 1,
+    "content": "Check Emails",
+    "iscomplete": true
+  },
+  {
+    "id": 2,
+    "content": "Do Homeworks",
+    "iscomplete": false
+  },
+  {
+    "id": 3,
+    "content": "Metting",
+    "iscomplete": true
+  }
+]
+
+// WE CAN CONVERT WITH SCRIPT TOO
+const todosJSON = JSON.stringify(todos);
