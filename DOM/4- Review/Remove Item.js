493e8d3d4e609bd001bc04d4511dbafc3e288b3d
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-07-10 22:19:28 +0430

    DOM: Remove Item Updated

--- DOM/4- Review/Remove Item.js
+++ DOM/4- Review/Remove Item.js
@@ -2,18 +2,6 @@ const itemList = document.querySelector('#items');
 const items = document.querySelectorAll('.list-group-item');
 const deleteButtons = document.querySelectorAll('.delete');
 
-console.log(deleteButtons);
-deleteButtons.forEach(addClickListener);
-
-function addClickListener(deleteButton) {
-  deleteButton.addEventListener('click', removeItem)
-}
-
-function removeItem(evnet) {
-  event.target.parentElement.remove();
-}
-
-//  ANOTHER WAY
 itemList.addEventListener('click', removeItems);
 
 function removeItems(event) {
